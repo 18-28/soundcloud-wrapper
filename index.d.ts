16b29698bd22516e31bab29d446459a33d280748
Zachary Sutton <zachary-sutton@outlook.com>
2025-03-10 15:26:45 +0000

    typed returns for some functions

--- index.d.ts
+++ index.d.ts
@@ -1,81 +1,81 @@
-declare module "soundcloud-wrapper" {
-  export default class SoundCloudClient {
-    constructor(clientId: string, clientSecret: string, redirectUri: string, PKCECodeVerifier: string)
+import * as Types from "./types"
 
-    tracks: {
-      getTrack(authToken: string, trackId: number): Promise<any>
-      getTrackStreams(authToken: string, trackId: number): Promise<any>
-      getTrackComments(authToken: string, trackId: number): Promise<any>
-      getTrackLikers(authToken: string, trackId: number): Promise<any>
-      getTrackReposters(authToken: string, trackId: number): Promise<any>
-      getRelatedTracks(authToken: string, trackId: number): Promise<any>
-      addComment(authToken: string, trackId: number, comment: { comment: { body: string; timestamp: number } }): Promise<any>
-      updateTrack(authToken: string, trackId: number, data: any): Promise<any>
-      deleteTrack(authToken: string, trackId: number): Promise<any>
-    }
+export default class SoundCloudClient {
+  constructor(clientId: string, clientSecret: string, redirectUri: string, PKCECodeVerifier: string)
 
-    playlists: {
-      getPlaylist(authToken: string, playlistId: number): Promise<any>
-      getPlaylistTracks(authToken: string, playlistId: number): Promise<any>
-      getPlaylistReposters(authToken: string, playlistId: number): Promise<any>
-      createPlaylist(authToken: string, playlistData: PlaylistData): Promise<any>
-      updatePlaylist(authToken: string, playlistId: number, playlistData: PlaylistData): Promise<any>
-      deletePlaylist(authToken: string, playlistId: number): Promise<any>
-    }
-
-    likes: {
-      likeTrack(authToken: string, trackId: number): Promise<any>
-      likePlaylist(authToken: string, playlistId: number): Promise<any>
-      unlikeTrack(authToken: string, trackId: number): Promise<any>
-      unlikePlaylist(authToken: string, playlistId: number): Promise<any>
-    }
-
-    me: {
-      me(authToken: string): Promise<any>
-      getActivity(authToken: string): Promise<any>
-      getTrackActivity(authToken: string): Promise<any>
-      getTrackLikes(authToken: string): Promise<any>
-      getPlaylistLikes(authToken: string): Promise<any>
-      getFollowings(authToken: string): Promise<any>
-      getFollowingsTracks(authToken: string): Promise<any>
-      followUser(authToken: string, userId: number): Promise<any>
-      unfollowUser(authToken: string, userId: number): Promise<any>
-    }
-
-    users: {
-      getUser(authToken: string, userId: number): Promise<any>
-      getUserFollowers(authToken: string, userId: number): Promise<any>
-      getUserFollowings(authToken: string, userId: number): Promise<any>
-      getUserPlaylists(authToken: string, userId: number): Promise<any>
-      getUserTracks(authToken: string, userId: number): Promise<any>
-      getUserWebProfiles(authToken: string, userId: number): Promise<any>
-      getUserLikedTracks(authToken: string, userId: number): Promise<any>
-      getUserLikedPlaylists(authToken: string, userId: number): Promise<any>
-    }
-
-    search: {
-      tracks(authToken: string, query: string): Promise<any>
-      playlists(authToken: string, query: string): Promise<any>
-      users(authToken: string, query: string): Promise<any>
-    }
-
-    token: {
-      getToken(PKCECodeChallenge: string): Promise<any>
-    }
-
-    misc: {
-      resolveUrl(authToken: string, url: string): Promise<any>
-    }
+  me: {
+    me(authToken: string): Promise<Types.Me>
+    getActivity(authToken: string): Promise<Types.Activities>
+    getTrackActivity(authToken: string): Promise<Types.Activities>
+    getTrackLikes(authToken: string): Promise<Types.Track[]>
+    getPlaylistLikes(authToken: string): Promise<Types.Playlist[]>
+    getFollowings(authToken: string): Promise<Types.Users>
+    getFollowingsTracks(authToken: string): Promise<Types.Track[]>
+    followUser(authToken: string, userId: number): Promise<{ status: string }>
+    unfollowUser(authToken: string, userId: number): Promise<{ status: string }>
   }
 
-  interface PlaylistData {
-    playlist: {
-      title: string
-      description: string
-      sharing: string
-      tracks: {
-        id: number
-      }[]
-    }
+  search: {
+    tracks(authToken: string, query: string): Promise<Types.Track[]>
+    playlists(authToken: string, query: string): Promise<Types.Playlist[]>
+    users(authToken: string, query: string): Promise<Types.User[]>
+  }
+
+  tracks: {
+    getTrack(authToken: string, trackId: number): Promise<any>
+    getTrackStreams(authToken: string, trackId: number): Promise<any>
+    getTrackComments(authToken: string, trackId: number): Promise<any>
+    getTrackLikers(authToken: string, trackId: number): Promise<any>
+    getTrackReposters(authToken: string, trackId: number): Promise<any>
+    getRelatedTracks(authToken: string, trackId: number): Promise<any>
+    addComment(authToken: string, trackId: number, comment: { comment: { body: string; timestamp: number } }): Promise<any>
+    updateTrack(authToken: string, trackId: number, data: any): Promise<any>
+    deleteTrack(authToken: string, trackId: number): Promise<any>
+  }
+
+  playlists: {
+    getPlaylist(authToken: string, playlistId: number): Promise<any>
+    getPlaylistTracks(authToken: string, playlistId: number): Promise<any>
+    getPlaylistReposters(authToken: string, playlistId: number): Promise<any>
+    createPlaylist(authToken: string, playlistData: PlaylistData): Promise<any>
+    updatePlaylist(authToken: string, playlistId: number, playlistData: PlaylistData): Promise<any>
+    deletePlaylist(authToken: string, playlistId: number): Promise<any>
+  }
+
+  likes: {
+    likeTrack(authToken: string, trackId: number): Promise<any>
+    likePlaylist(authToken: string, playlistId: number): Promise<any>
+    unlikeTrack(authToken: string, trackId: number): Promise<any>
+    unlikePlaylist(authToken: string, playlistId: number): Promise<any>
+  }
+
+  users: {
+    getUser(authToken: string, userId: number): Promise<any>
+    getUserFollowers(authToken: string, userId: number): Promise<any>
+    getUserFollowings(authToken: string, userId: number): Promise<any>
+    getUserPlaylists(authToken: string, userId: number): Promise<any>
+    getUserTracks(authToken: string, userId: number): Promise<any>
+    getUserWebProfiles(authToken: string, userId: number): Promise<any>
+    getUserLikedTracks(authToken: string, userId: number): Promise<any>
+    getUserLikedPlaylists(authToken: string, userId: number): Promise<any>
+  }
+
+  token: {
+    getToken(PKCECodeChallenge: string): Promise<any>
+  }
+
+  misc: {
+    resolveUrl(authToken: string, url: string): Promise<any>
+  }
+}
+
+interface PlaylistData {
+  playlist: {
+    title: string
+    description: string
+    sharing: string
+    tracks: {
+      id: number
+    }[]
   }
 }
---
+++ types.d.ts
@@ -0,0 +1,205 @@
+export interface Me {
+  avatar_url: string
+  city: string
+  country: string
+  created_at: string
+  description: string
+  discogs_name: string
+  first_name: string
+  followers_count: number
+  followings_count: number
+  full_name: string
+  id: number
+  kind: string
+  last_modified: string
+  last_name: string
+  likes_count: number
+  locale: string
+  online: boolean
+  permalink: string
+  permalink_url: string
+  plan: string
+  playlist_count: number
+  primary_email_confirmed: boolean
+  private_playlists_count: number
+  private_tracks_count: number
+  public_favorites_count: number
+  quota: {
+    unlimited_upload_quota: boolean
+    upload_seconds_used: number
+    upload_seconds_left: number
+  }
+  reposts_count: number
+  subscriptions: Array<{
+    product: {
+      id: string
+      name: string
+    }
+    recurring: boolean
+  }>
+  track_count: number
+  upload_seconds_left: number
+  uri: string
+  username: string
+  website: string
+  website_title: string
+}
+
+export interface User {
+  avatar_url: string
+  id: number
+  kind: string
+  permalink_url: string
+  uri: string
+  username: string
+  permalink: string
+  created_at: string
+  last_modified: string
+  first_name: string
+  last_name: string
+  full_name: string
+  city: string
+  description: string
+  country: string | null
+  track_count: number
+  public_favorites_count: number
+  reposts_count: number
+  followers_count: number
+  followings_count: number
+  plan: string
+  myspace_name: string | null
+  discogs_name: string | null
+  website_title: string
+  website: string
+  comments_count: number
+  online: boolean
+  likes_count: number
+  playlist_count: number
+  subscriptions: Array<{
+    product: {
+      id: string
+      name: string
+    }
+  }>
+}
+
+export interface Users {
+  collection: User[]
+  next_href: string
+}
+
+export interface Activities {
+  collection: {
+    type: "track" | "playlist" | "track:repost"
+    created_at: string
+    origin: Track | Playlist
+  }[]
+  next_href: string
+  future_href: string
+}
+
+export interface Track {
+  duration: number
+  genre: string
+  release_day: number
+  permalink: string
+  permalink_url: string
+  release_month: number
+  release_year: number
+  description: string
+  uri: string
+  label_name: string
+  label_id: number | null
+  label: string | null
+  tag_list: string
+  track_count: number
+  user_id: number
+  last_modified: string
+  license: string
+  user: User
+  playlist_type: string
+  type: string
+  id: number
+  downloadable: boolean | null
+  likes_count: number
+  sharing: string
+  created_at: string
+  release: string | null
+  tags: string
+  kind: string
+  title: string
+  purchase_title: string | null
+  ean: string
+  streamable: boolean
+  embeddable_by: string
+  artwork_url: string
+  purchase_url: string
+  tracks_uri: string
+  permalink_url: string
+  artwork_url: string
+  stream_url: string
+  download_url: string | null
+  waveform_url: string
+  available_country_codes: string[] | null
+  secret_uri: string | null
+  user_favorite: boolean
+  user_playback_count: number
+  playback_count: number | null
+  download_count: number | null
+  favoritings_count: number | null
+  reposts_count: number | null
+  downloadable: boolean
+  access: string
+  policy: string | null
+  monetization_model: string | null
+}
+
+export interface Tracks {
+  collection: Track[]
+  next_href: string
+}
+
+export interface Playlist {
+  duration: number
+  genre: string
+  release_day: number
+  permalink: string
+  permalink_url: string
+  release_month: number
+  release_year: number
+  description: string
+  uri: string
+  label_name: string
+  label_id: number | null
+  label: string | null
+  tag_list: string
+  track_count: number
+  user_id: number
+  last_modified: string
+  license: string
+  user: User
+  playlist_type: string
+  type: string
+  id: number
+  downloadable: boolean | null
+  likes_count: number
+  sharing: string
+  created_at: string
+  release: string | null
+  tags: string
+  kind: string
+  title: string
+  purchase_title: string | null
+  ean: string
+  streamable: boolean
+  embeddable_by: string
+  artwork_url: string
+  purchase_url: string
+  tracks_uri: string
+  tracks: Track[]
+}
+
+export interface Playlists {
+  collection: Playlist[]
+  next_href: string
+}
